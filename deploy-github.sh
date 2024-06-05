1f873c467467a4ce50d10f13ca2028e7c7956337
Hajaniaina Rafaliarintsoa <hajaniaina.rafaliarintsoa@universign.com>
2024-06-05 14:37:30 +0200

    Deploy to GitHub Pages

--- deploy-github.sh
+++ deploy-github.sh
@@ -1,1 +1,1 @@
-yarn run build && git add . && git commit -m "Deploy to GitHub Pages" && git push --force -u origin gh-page
+yarn run build && git add . && git commit -m "Deploy to GitHub Pages" && git push --force -u origin main
